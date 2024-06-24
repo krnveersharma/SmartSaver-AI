06575821808cff3dae92f04b62409501ba5cec04
krnveersharma <115925693+krnveersharma@users.noreply.github.com>
2024-06-24 20:58:41 +0530

    Added yourblogs feature

---
+++ src/app/pages/addblog/page.tsx
@@ -0,0 +1,102 @@
+"use client";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  FormControl,
+  FormLabel,
+  Heading,
+  Text,
+  Textarea,
+  useStatStyles,
+} from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const AddBlog = () => {
+  const router=useRouter()
+  const [data, setData] = useState({
+    title: "",
+    category: "",
+    description: "",
+  });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await fetch("/api/Blog", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    setData({
+      title: "",
+      category: "",
+      description: "",
+    });
+    router.push('/pages/yourblogs')
+  };
+  return (
+    <Card>
+      <CardHeader>
+        <Heading>Add Your Blog!!!</Heading>
+      </CardHeader>
+      <CardBody>
+        <form onSubmit={handleSubmit}>
+          <FormControl>
+            <FormLabel fontSize={"large"} fontWeight={"bold"}>
+              Title
+            </FormLabel>
+            <Textarea
+              maxLength={480}
+              value={data.title}
+              onChange={(e) => {
+                setData((prev) => ({
+                  ...prev,
+                  title: e.target.value,
+                }));
+              }}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel fontSize={"large"} fontWeight={"bold"}>
+              Category
+            </FormLabel>
+            <Textarea
+              maxLength={20}
+              value={data.category}
+              onChange={(e) => {
+                setData((prev) => ({
+                  ...prev,
+                  category: e.target.value,
+                }));
+              }}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel fontSize={"large"} fontWeight={"bold"}>
+              Description
+            </FormLabel>
+            <Textarea
+              value={data.description}
+              onChange={(e) => {
+                setData((prev) => ({
+                  ...prev,
+                  description: e.target.value,
+                }));
+              }}
+            />
+          </FormControl>
+          <br />
+          <Button float={"right"} colorScheme={"blue"} type="submit">
+            Submit
+          </Button>
+        </form>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default AddBlog;
---
+++ src/app/pages/yourblogs/page.tsx
@@ -0,0 +1,70 @@
+'use client'
+import { Box, Button, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
+import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+
+const YourBlogs = () => {
+    const router = useRouter()
+
+    const [data,setData]=useState([{
+            id:'',
+            title:'',
+            Category:'',
+            Description:'',
+            userId:''
+    }])
+    useEffect(()=>{
+        const getBlogs=async()=>{
+            const response=await fetch('/api/userblogs',{
+                method:'GET'
+            })
+            const allBlogs=await response.json();
+            setData(allBlogs.userblogs);
+        }
+        getBlogs();
+    },[])
+  return (
+    <Box>
+    <TableContainer>
+        <Table variant={'simple'}>
+            <Thead>
+                <Tr>
+                    <Th isTruncated>Title</Th>
+                    <Th isTruncated>Category</Th>
+                    <Th isTruncated>Description</Th>
+                </Tr>
+            </Thead>
+            <Tbody>
+                {data.map((item)=>{
+                    return(<Tr>
+                        <Td>{item.title}</Td>
+                        <Td>{item.Category}</Td>
+                        <Td>{item.Description}</Td>
+                        <Td><Button colorScheme={'red'} onClick={async(e)=>{
+                            const response=await fetch('/api/userblogs',{
+                                method:"DELETE",
+                                headers:{
+                                    'Content-Type':'application/json'
+                                  },
+                                  body:JSON.stringify(item.id)
+                            })
+                            const newData=await response.json()
+                            setData(newData.data)
+                        }}
+                        
+                        >Delete</Button></Td>
+                    </Tr>)
+                })}
+                
+            </Tbody>
+            
+        </Table>
+        
+    </TableContainer>
+    <br/><br/>
+    <Button background={'black'} color={'white'} onClick={()=>router.push('/pages/addblog')}>+Add Blog</Button>
+    </Box>
+  )
+}
+
+export default YourBlogs
